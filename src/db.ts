f449271daf621ac02ce9633b6ef406347164da4b
Noah Halstead <nhalstead@users.noreply.github.com>
2025-03-11 22:52:24 -0400

    Spell check, fix license metadata in package lock

--- src/db.ts
+++ src/db.ts
@@ -7,7 +7,7 @@ declare global {
 
 // This is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
-// create a new connectison to the DB with every change either.
+// create a new connection to the DB with every change either.
 if (process.env.NODE_ENV !== "development") {
   prismaClient = new PrismaClient();
   prismaClient.$connect();
